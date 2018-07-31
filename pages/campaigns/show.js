ae151880090116dcffc5c505a538c7cf4e925391
shane554 <shanemd26@gmail.com>
2018-07-31 16:10:12 +0100

    Add files via upload

---
+++ pages/campaigns/show.js
@@ -0,0 +1,17 @@
+// we want to show users info about the campaign
+import React, { Component } from 'react';
+import Layout from '../../components/Layout'
+
+class CampaignShow extends Component {
+    render() {
+        return(
+            // we want to wrap layout around component
+            <Layout>
+            <h3>Campaign Show</h3>
+            </Layout>
+
+        );
+    }
+}
+
+export default CampaignShow;
