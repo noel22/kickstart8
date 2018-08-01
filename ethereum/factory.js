2d6ce85ec5694ed433edbbf982141ce84279e6bf
shane554 <shanemd26@gmail.com>
2018-08-01 16:34:11 +0100

    Add files via upload

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,8 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x57163F6012B0e409d4DF6c0AceD6976Cf8bA6118'
+    // address of new  deployed factory instance
+    '0x312252DFDdFBdD43d9590c1EEc8F57412D880D9B'
 );
 
 // we need access to our web 3 instance
